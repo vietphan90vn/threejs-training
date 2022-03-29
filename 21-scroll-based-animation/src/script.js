9335d38b0c808e650066654b667cee6daa2e0ee7
Viet Phan <viet.phanho@asnet.com.vn>
2022-03-29 11:17:25 +0700

    Add Unit 21-Scroll By animation.

---
+++ 21-scroll-based-animation/src/script.js
@@ -0,0 +1,213 @@
+import "./style.css";
+import * as THREE from "three";
+import * as dat from "lil-gui";
+import gsap from "gsap";
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI();
+
+const parameters = {
+    materialColor: "#ffeded",
+};
+
+gui.addColor(parameters, "materialColor").onChange(() => {
+    material.color.set(parameters.materialColor);
+});
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Test cube
+ */
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial({ color: "#ff0000" })
+// );
+// scene.add(cube);
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Group
+const cameraGroup = new THREE.Group();
+scene.add(cameraGroup);
+
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+    35,
+    sizes.width / sizes.height,
+    0.1,
+    100
+);
+camera.position.z = 6;
+cameraGroup.add(camera);
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    alpha: true,
+});
+renderer.setClearAlpha(0);
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Objects
+ */
+// Material
+
+// Texture
+const textureLoader = new THREE.TextureLoader();
+const gradientTexture = textureLoader.load("textures/gradients/3.jpg");
+gradientTexture.magFilter = THREE.NearestFilter;
+
+const material = new THREE.MeshToonMaterial({
+    color: parameters.materialColor,
+    gradientMap: gradientTexture,
+});
+
+// Meshes
+const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material);
+const mesh2 = new THREE.Mesh(new THREE.ConeGeometry(1, 2, 32), material);
+const mesh3 = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+    material
+);
+
+mesh1.scale.set(0.5, 0.5, 0.5);
+
+mesh3.position.y = -2;
+mesh3.scale.set(0.5, 0.5, 0.5);
+
+const objectsDistance = 4;
+mesh1.position.y = -objectsDistance * 0;
+mesh2.position.y = -objectsDistance * 1;
+mesh3.position.y = -objectsDistance * 2;
+
+mesh1.position.x = 2;
+mesh2.position.x = -2;
+mesh3.position.x = 2;
+
+const sectionMeshes = [mesh1, mesh2, mesh3];
+
+scene.add(mesh1, mesh2, mesh3);
+
+/**
+ * Lights
+ */
+const directionalLight = new THREE.DirectionalLight("#ffffff", 1);
+directionalLight.position.set(1, 1, 0);
+scene.add(directionalLight);
+
+/**
+ * Scroll
+ */
+let scrollY = window.scrollY;
+let currentSection = 0;
+window.addEventListener("scroll", () => {
+    scrollY = window.scrollY;
+    const newSection = Math.round(scrollY / sizes.height);
+
+    if (newSection != currentSection) {
+        currentSection = newSection;
+
+        gsap.to(sectionMeshes[currentSection].rotation, {
+            duration: 1.5,
+            ease: "power2.inOut",
+            x: "+=6",
+            y: "+=3",
+            z: "+=1.5",
+        });
+    }
+});
+
+/**
+ * Cursor
+ */
+const cursor = {};
+cursor.x = 0;
+cursor.y = 0;
+
+window.addEventListener("mousemove", (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5;
+    cursor.y = event.clientY / sizes.height - 0.5;
+});
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+let previousTime = 0;
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime();
+    const deltaTime = elapsedTime - previousTime;
+    previousTime = elapsedTime;
+
+    // Animate meshes
+    for (const mesh of sectionMeshes) {
+        mesh.rotation.x += deltaTime * 0.1;
+        mesh.rotation.y += deltaTime * 0.12;
+    }
+
+    const cameraPositionY =
+        (-scrollY / sizes.height) *
+        0.5 *
+        objectsDistance *
+        (sectionMeshes.length - 1);
+
+    const targetPosition = camera.position;
+    targetPosition.y = cameraPositionY;
+
+    // Animate camera
+    camera.position.lerp(targetPosition, deltaTime * 0.1);
+
+    const parallaxX = cursor.x * 0.5;
+    const parallaxY = -cursor.y * 0.5;
+
+    cameraGroup.position.x +=
+        (parallaxX - cameraGroup.position.x) * deltaTime * 5;
+    cameraGroup.position.y +=
+        (parallaxY - cameraGroup.position.y) * deltaTime * 5;
+
+    // Render
+    renderer.render(scene, camera);
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick);
+};
+
+tick();
